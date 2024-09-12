b94fc1cff1d5777c0becd675c21109a0aad0f49c
羽 王 <1344695152@qq.com>
2024-09-12 16:51:19 +0800

    提交代码

--- WPF.Reflex/MainWindow.xaml.cs
+++ WPF.Reflex/MainWindow.xaml.cs
@@ -13,5 +13,35 @@ namespace WPF.Reflex
             InitializeComponent();
             this.DataContext = App.Current.Services.GetService<MainWindowViewModel>(); 
         }
+        private bool isDarkMode = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isDarkMode)
+            {
+                SwitchToLightMode();
+            }
+            else
+            {
+                SwitchToDarkMode();
+            }
+            isDarkMode = !isDarkMode;
+        }
+
+        public void SetTheme(Uri themeUri)
+        {
+            // 清空当前资源字典
+            ResourceDictionary newTheme = new ResourceDictionary { Source = themeUri };
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(newTheme);
+        }
+
+        private void SwitchToDarkMode()
+        {
+            SetTheme(new Uri("Theme/DarkTheme.xaml", UriKind.Relative));
+        }
+        private void SwitchToLightMode()
+        {
+            SetTheme(new Uri("Theme/LightTheme.xaml", UriKind.Relative));
+        }
     }
 }
